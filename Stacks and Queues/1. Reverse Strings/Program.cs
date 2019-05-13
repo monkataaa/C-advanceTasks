d442709a53cf2d2a1f67bbfda3f1388d8d1a00a2
monkata <monkataaa@abv.bg>
2019-05-13 17:45:54 +0300

    added Task: Reverse Strings, added NUmbers to all tasks

---
+++ Stacks and Queues/1. Reverse Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stack = new Stack<char>();
+            var input = Console.ReadLine();
+
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+        }
+    }
+}
