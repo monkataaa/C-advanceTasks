1bd10661c14d5c071d6496039442212ccdc1475d
monkata <monkataaa@abv.bg>
2019-05-13 17:53:18 +0300

    added task: 2. Stack Sum

---
+++ Stacks and Queues/2. Stack Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            var commandInfo = Console.ReadLine().ToLower();
+
+            while (commandInfo != "end")
+            {
+                var tokens = commandInfo.Split();
+                var command = tokens[0].ToLower();
+
+                if (command == "add")
+                {
+                    stack.Push(int.Parse(tokens[1]));
+                    stack.Push(int.Parse(tokens[2]));
+                }
+                var removeNumebrs = int.Parse(tokens[1]);
+                if (command == "remove" && removeNumebrs < stack.Count())
+                {
+                    for (int i = 0; i < removeNumebrs; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+                commandInfo = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+
+
+
+        }
+    }
+}
