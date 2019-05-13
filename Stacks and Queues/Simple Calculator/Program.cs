39b13819b56321c694d4cfb8604564132579d7f0
monkata <monkataaa@abv.bg>
2019-05-13 17:33:18 +0300

    added Stacks An Queues Folder and Task: Simple Calculator

---
+++ Stacks and Queues/Simple Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var stack = new Stack<string>(Console.ReadLine().Split().Reverse());
+            var result = 0;
+
+            result += int.Parse(stack.Pop());
+
+            while (stack.Any())
+            {
+                
+                
+                if (stack.Peek() == "+")
+                {
+                    stack.Pop();
+                    result += int.Parse(stack.Pop());
+                }
+                else if (stack.Peek() == "-")
+                {
+                    stack.Pop();
+                    result -= int.Parse(stack.Pop());
+                }
+                
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
